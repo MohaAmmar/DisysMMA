894dd915e2370d035d6fe2608a7f3a584f862936
Noerklit <mchrnj@gmail.com>
2023-09-20 12:15:47 +0200

    Updated solution, 1 is now solved

--- Handin_2/main.go
+++ Handin_2/main.go
@@ -6,69 +6,48 @@ import (
 	"time"
 )
 
-func client(comms chan int) {
-	var counter int = 0
+func client(counter int, comms chan Data) {
 	var seqC int = rand.Intn(10)
 	var ackC int
 
-	/*if counter == 0 {
-		clientFirstListen(seqC, comms, wg)
-		counter++
-	} else {
-		clientListen(seqC, ackC, comms, wg)
-	}*/
-
 	if counter == 0 {
 		fmt.Printf("seqC is %d \n", seqC)
-		comms <- seqC
-		counter++
+		data := Data{seqC, 0, "Hello"};
+		comms <- data
 	} else {
-		<-comms
-		ackC = <-comms
-		seqC = <-comms
+		newData := <- comms
+		ackC = newData.seq + 1
 		fmt.Printf("ackC is %d \n", ackC)
-		fmt.Printf("seqC is %d \n", seqC)
-		<-comms
-		comms <- ackC
-		comms <- seqC
+		fmt.Printf("seqC is %d \n", newData.ack)
 	}
 
 }
 
-func server(comms chan int) {
+func server(comms chan Data) {
 	var seqS int = rand.Intn(10)
 	var ackS int
 
-	ackS = <-comms
+	data := <-comms 
+	ackS = data.seq + 1
 	fmt.Printf("ackS is %d \n", ackS)
 	fmt.Printf("seqS is %d \n", seqS)
-	comms <- ackS
-	comms <- seqS
+	newData := Data{seqS, ackS, "Hello"};
+	comms <- newData
+	client(1, comms)
 }
 
-/*func clientFirstListen(seqC int, comms chan int, wg *sync.WaitGroup) {
-	comms <- seqC
+type Data struct {
+	seq int
+	ack int
+	msg string
 }
-
-func clientListen(seqC int, ackC int, comms chan int, wg *sync.WaitGroup) (int, int){
-		ackC := <-comms
-		seqC := <-comms
-		<-comms
-		comms <- ackC
-		comms <- seqC
-	return ackC, seqC
-}
-
-func serverListen(seqS int, ackS int, comms chan int, wg *sync.WaitGroup) {
-
-}*/
 
 func main() {
 
-	var comms = make(chan int, 2)
+	var comms = make(chan Data, 2)
 
-	go client(comms)
+	go client(0, comms)
 	go server(comms)
 
-	time.Sleep(100 * time.Second)
+	time.Sleep(2 * time.Second)
 }
